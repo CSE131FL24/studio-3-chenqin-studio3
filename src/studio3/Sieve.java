ee4aefc32c8091fb979c0f7a4cd589b5450620f4
Anthony Qin <anthony.qin9@gmail.com>
2024-09-18 17:50:23 -0500

    Finished Studio 3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -5,22 +5,31 @@ import java.util.Scanner;
 public class Sieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.println("n: ");
+		System.out.print("n: ");
 		int n = in.nextInt();
 		int[] arr = new int[n];
-		for (i = 0; i < n; i++) {
+
+		for (int i = 0; i < n; i++) {
 			arr[i] = i + 1;
 		}
-		
+
+		for (int i = 1; i < n; i++) {
+			if (arr[i] != 0) {
+				for (int j = i + arr[i]; j < n; j = j + arr[i]) {
+					arr[j] = 0;
+					}
+				}
+			}
+
 		for (int i = 0; i < n; i++) {
-			for (int j = 2 * i; j < n; j = j + i) {
-				arr[j] = 0;
+			if (arr[i] != 0) {
+				System.out.print(arr[i] + " ");
 			}
 		}
-		
-		
-	}
+
+
+
+		}
 
 }
